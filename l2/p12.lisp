5c7287e950a9bc9520b0fb3e26b91f77a3865efa
George <georgecamilar2@gmail.com>
2020-12-25 17:40:00 +0200

    Update to lab 2 lisp

--- l2/p12.lisp
+++ l2/p12.lisp
@@ -2,12 +2,48 @@
 ;;  preordine.
 
 ;; De exemplu arborele
-;; A
-;; / \
-;; B C
+;;   A
 ;;  / \
-;; D E
+;; B   C
+;;    / \
+;;   D   E
 ;; se poate reprezenta astfel in cele doua moduri:
 ;; (A 2 B 0 C 2 D 0 E 0) (1)
 ;; (A (B) (C (D) (E))) (2)
 
+(defun preordine (L)
+    (cond
+        ((null L) (list))
+        ((listp(car L))(append (preordine (car L)) (preordine (cdr L))))
+        (T ( cons (car L) (preordine (cdr L)) ))
+    )
+)
+
+(defun preorder (L)
+    (write L)
+    (terpri)
+    (cond
+        ((null L)(list))
+        (T
+            (cons ( car L ) ( append (preorder (cadr L)) (preorder (caddr L)) ) )
+        )
+    )
+)
+
+(defun mappreorder(L)
+    (write L)
+    (terpri)
+    (cond 
+        ((numberp L) (list L))
+        ((atom L) (list L))
+        (T (mapcan #'mappreorder L ) )
+    )
+)
+
+(defun negative(L)
+    (cond
+        ((null L) 0)
+        ((and (numberp (car L)) (< 0 (car L))) (+ 1 (negative(cdr L))))
+        (T (negative (cdr L)) )
+    )
+)
